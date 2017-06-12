f2189b37cd19345ae23291aa3a548dd4d3b85af0
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-12 08:48:55 +0700

    simple array

---
+++ a.js
@@ -0,0 +1,9 @@
+// let a = new Array();
+let arr = [1, 4, 6, 'NodeJS', 'iOS'];
+
+console.log(arr.length);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+console.log(arr.join('|||'));
